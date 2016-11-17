da9391f537793c011418f318ebb12ab8ac208aac
Balint Erdi <balint.erdi@gmail.com>
2016-11-17 10:48:28 +0100

    Include resource linkage data with friendships
    That cuts down on the # of xhr requests that needs to be made to fetch
    all friendships of a person

--- app/resources/friendship_resource.rb
+++ app/resources/friendship_resource.rb
@@ -1,5 +1,5 @@
 class FriendshipResource < JSONAPI::Resource
-  has_one :friender
-  has_one :friended
+  has_one :friender, always_include_linkage_data: true
+  has_one :friended, always_include_linkage_data: true
   attributes :strength
 end
--- app/resources/person_resource.rb
+++ app/resources/person_resource.rb
@@ -6,19 +6,5 @@ class PersonResource < JSONAPI::Resource
   relationship :friended_friendships, to: :many, class_name: "Friendship"
   relationship :friendships, to: :many, class_name: "Friendship"
 
-  ## TODO: when there is an `includes` parameter, it will eventually
-  # get to JSONAPI::Resource#apply_includes and call the `includes` method
-  # on the `records`, which is the Person class. That will fail as it really
-  # needs a relationship to be defined on the model
-  # So my current idea is to use a custom `friendships` link instead and return
-  # the correct, merged data there
-  # def friendships(options={})
-  #   puts "XXX PersonResource#frienships"
-  #   Friendship.where("friender_id = ? OR friended_id = ?", id, id)
-  # end
-
-  def custom_links(options)
-    { friendships: options[:serializer].link_builder.self_link(self) + "/friendships" }
-  end
 end
 
