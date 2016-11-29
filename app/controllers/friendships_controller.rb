4e66a2db9e36d970f37bcb6029b98343a2841cff
Balint Erdi <balint.erdi@gmail.com>
2016-11-29 15:57:19 +0100

    Remove superfluous get_related_resources method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,10 @@
 class FriendshipsController < JSONAPI::ResourceController
-  def get_related_resources
-    person = Person.find(params[:person_id])
-    friendship_resources = person.friendships.map do |friendship|
-      FriendshipResource.new(friendship, context)
-    end
-    resource_serializer = JSONAPI::ResourceSerializer.new(FriendshipResource)
-    render json: resource_serializer.serialize_to_hash(friendship_resources)
-  end
+  # def get_related_resources
+  #   person = Person.find(params[:person_id])
+  #   friendship_resources = person.friendships.map do |friendship|
+  #     FriendshipResource.new(friendship, context)
+  #   end
+  #   resource_serializer = JSONAPI::ResourceSerializer.new(FriendshipResource)
+  #   render json: resource_serializer.serialize_to_hash(friendship_resources)
+  # end
 end
